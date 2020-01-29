29490d6941e7460a65c0be2e4767a33e2c2dc37a
woohyeon Ahn <europeluv@gmail.com>
2020-01-29 23:23:20 +0900

    [Easy] 20. Valid Parentheses

--- cpp/20_ValidParentheses/20_ValidParentheses.cpp
+++ cpp/20_ValidParentheses/20_ValidParentheses.cpp
@@ -1,10 +1,87 @@
+/*
+    Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+    An input string is valid if:
+
+    1. Open brackets must be closed by the same type of brackets.
+    2. Open brackets must be closed in the correct order.
+    
+    Note that an empty string is also considered valid.
+
+    Example 1:
+    Input: "()"
+    Output: true
+    
+    Example 2:
+    Input: "()[]{}"
+    Output: true
+    
+    Example 3:
+    Input: "(]"
+    Output: false
+    
+    Example 4:
+    Input: "([)]"
+    Output: false
+    
+    Example 5:
+    Input: "{[]}"
+    Output: true
+*/
+
 #include <string>
+#include <stack>
 
 using namespace std;
 
 class Solution {
 public:
     bool isValid(string s) {
+        /*
+            스택을 이용하여 문자열의 크기만큼 루프를 돕니다.
+            루프를 돌면서 각 문자를 스택에 push합니다.
+        */
 
+        size_t sSize = s.size();
+        if (sSize == 0)
+            return true;
+        if ((sSize & 1) == 1)
+            return false;
+
+        stack<char> stk;
+        for (size_t i = 0; i != sSize; ++i)
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+            {
+                stk.emplace(s[i]);
+                continue;
+            }
+            else if (s[i] == ')')
+            {
+                if (stk.empty() || stk.top() != '(')
+                    return false;
+
+                stk.pop();
+                continue;
+            }
+            else if (s[i] == '}')
+            {
+                if (stk.empty() || stk.top() != '{')
+                    return false;
+
+                stk.pop();
+                continue;
+            }
+            else if (s[i] == ']')
+            {
+                if (stk.empty() || stk.top() != '[')
+                    return false;
+
+                stk.pop();
+                continue;
+            }
+        }
+
+        return stk.empty();
     }
 };
