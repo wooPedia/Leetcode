42c8e249fb9ba2ff63aa36f803be8000a7e39790
Woohyeon Ahn <46551002+wooPedia@users.noreply.github.com>
2020-02-01 20:51:50 +0900

    [Easy] 20.Valid Parentheses

--- cpp/20_ValidParentheses/20_ValidParentheses.cpp
+++ cpp/20_ValidParentheses/20_ValidParentheses.cpp
@@ -1,9 +1,6 @@
 /*
-<<<<<<< HEAD
-=======
     [Easy] 20.Valid Parentheses
     -----------------------------------------------------------------------------------------------------------------------
->>>>>>> 64ec1a41a743be28bc2427fb6ad41dbf467b00f6
     Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
     An input string is valid if:
@@ -32,13 +29,10 @@
     Example 5:
     Input: "{[]}"
     Output: true
-<<<<<<< HEAD
-=======
     -----------------------------------------------------------------------------------------------------------------------
     
     - Detail
     https://woo-dev.tistory.com/73
->>>>>>> 64ec1a41a743be28bc2427fb6ad41dbf467b00f6
 */
 
 #include <string>
@@ -49,17 +43,8 @@ using namespace std;
 class Solution 
 {
 public:
-<<<<<<< HEAD
-    bool isValid(string s) {
-        /*
-            스택을 이용하여 문자열의 크기만큼 루프를 돕니다.
-            루프를 돌면서 각 문자를 스택에 push합니다.
-        */
-=======
     bool isValid(string s) 
     {
->>>>>>> 64ec1a41a743be28bc2427fb6ad41dbf467b00f6
-
         size_t sSize = s.size();
         if (sSize == 0)
             return true;
